52ff171bb19c30cdc68c609a835a1cd9ef388658
jjong <whdgh9588@gmail.com>
2024-09-04 16:37:06 +0900

    0904React & security-token

---
+++ app3/src/Router.js
@@ -0,0 +1,19 @@
+
+import { Routes, Route } from "react-router-dom"
+import Userhome from "./components/member/Userhome"
+import Login from "./components/member/Login"
+import Info from "./components/member/Info"
+import List from "./components/board/List"
+import Add from "./components/board/Add"
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/userhome" element={<Userhome />} />
+      <Route path="/member/login" element={<Login />} />
+      <Route path="/member/info" element={<Info />} />
+      <Route path="/board/list" element={<List />} />
+      <Route path="/board/add" element={<Add />} />
+    </Routes>
+  )
+}
